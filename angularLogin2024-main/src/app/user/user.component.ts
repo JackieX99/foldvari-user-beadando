0b4f71ca51fcebc710b34f18576d8fef0501d398
Földvári Alex <43179105+JackieX99@users.noreply.github.com>
2024-03-08 00:14:35 +0100

    kész feladat

---
+++ angularLogin2024-main/src/app/user/user.component.ts
@@ -0,0 +1,61 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent implements OnInit {
+
+  userData!: User;
+
+  constructor(
+    private route: ActivatedRoute,
+    private http: HttpClient
+  ) { }
+
+  id: string = "-1";
+
+  ngOnInit(): void {
+    this.id = this.route.snapshot.paramMap.get('id')!
+    //alert(this.id)
+    this.getUser(this.id)
+  }
+
+  getUser(id: string){
+    let url = "https://jsonplaceholder.typicode.com/users/" + id;
+    this.http.get<User>(url).subscribe(
+      res=>{
+        this.userData = res;
+      }
+    )
+  }
+
+}
+
+//Megcsinálni a https://jsonplaceholder.typicode.com/users/1 responsa alapján
+type User = {
+  id: number,
+  name: string,
+  username: string,
+  email: string,
+    address: {
+    street: string,
+    suite: string,
+    city: string,
+    zipcode: string,
+    geo: {
+      lat: string,
+      lng: string
+    }
+  },
+  phone: string,
+  website: string,
+  company: {
+    name: string,
+    catchPhrase: string,
+    bs: string
+  }
+}
